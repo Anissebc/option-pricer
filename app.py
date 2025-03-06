e3198cee614b861dca42699a75a6541b8abfd70a
Anisse <anisse@Host-003.lan>
2025-03-06 10:59:59 +0100

    Correction port Render

--- app.py
+++ app.py
@@ -90,5 +90,5 @@ def option_pricer():
     return render_template_string(HTML_TEMPLATE, result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
 
